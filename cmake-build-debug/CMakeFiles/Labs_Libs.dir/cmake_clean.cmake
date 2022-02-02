e062c2525d2540d8840955446dd70dd0b3d33663
Fadet <tntboxmc@gmail.com>
2022-02-02 20:14:57 +0300

    Hello, world!

---
+++ cmake-build-debug/CMakeFiles/Labs_Libs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labs_Libs.dir/main.c.obj"
+  "Labs_Libs.exe"
+  "Labs_Libs.exe.manifest"
+  "Labs_Libs.pdb"
+  "libLabs_Libs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Labs_Libs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Labs_Libs.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Labs_Libs.
+# This may be replaced when dependencies are built.
