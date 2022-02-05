bb5efb3b1dc68bc09a9f706e39cc2285c6aafb74
Fadet <tntboxmc@gmail.com>
2022-02-05 23:39:38 +0300

    memory usage of vectorVoid

--- libs/data_structures/vectorVoid/vectorVoid.c
+++ libs/data_structures/vectorVoid/vectorVoid.c
@@ -3,3 +3,51 @@
 //
 
 #include "vectorVoid.h"
+#include <stdio.h>
+
+static void raise_bad_allocation() {
+    fprintf(stderr, "bad allocation\n");
+    exit(1);
+}
+
+static void raise_empty_vector() {
+    fprintf(stderr, "vector is empty\n");
+    exit(1);
+}
+
+static void raise_index_error(const size_t index) {
+    fprintf(stderr, "IndexError: a[%llu] doesn't exist\n", index);
+    exit(1);
+}
+
+static size_t min2(const size_t x, const size_t y) {
+    return x < y ? x : y;
+}
+
+vectorVoid createVectorV(const size_t n, const size_t baseTypeSize) {
+    void *data = malloc(baseTypeSize * n);
+    if (NULL == data)
+        raise_bad_allocation();
+
+    return (vectorVoid) {(n == 0 ? NULL : data), 0, n};
+}
+
+void reserveV(vectorVoid * const v, const size_t newCapacity) {
+    v->capacity = newCapacity;
+    v->size = min2(v->size, v->capacity);
+    v->data = realloc(v->data, v->baseTypeSize * newCapacity);
+    if (NULL == v->data && newCapacity != 0)
+        raise_bad_allocation();
+}
+
+void clearV(vectorVoid * const v) {
+    v->size = 0;
+}
+
+void shrinkToFitV(vectorVoid * const v) {
+    reserveV(v, v->size);
+}
+
+void deleteVectorV(vectorVoid * const v) {
+    reserveV(v, 0);
+}
--- libs/data_structures/vectorVoid/vectorVoid.h
+++ libs/data_structures/vectorVoid/vectorVoid.h
@@ -6,6 +6,7 @@
 #define LABS_LIBS_VECTORVOID_H
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct vectorVoid{
     void *data;
@@ -13,5 +14,22 @@ typedef struct vectorVoid{
     size_t capacity;
     size_t baseTypeSize;
 } vectorVoid;
+
+// returns vector's descriptor of size n
+// baseTypeSize represents a number of bytes which given type contains
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+// changes the capacity of v to newCapacity
+// reallocates the data of v (memory pointer is NULL if newCapacity equals 0)
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+// sets size of v to zero
+void clearV(vectorVoid *v);
+
+// shrinks the capacity of v to its size
+void shrinkToFitV(vectorVoid *v);
+
+// frees the data of v
+void deleteVectorV(vectorVoid *v);
 
 #endif //LABS_LIBS_VECTORVOID_H
