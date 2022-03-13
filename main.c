dbc074775e0c38a16b99847818e6e6bd5c168874
Fadet <tntboxmc@gmail.com>
2022-03-13 19:30:49 +0300

    added sorts library' template

--- main.c
+++ main.c
@@ -2,17 +2,16 @@
 //#include "libs/data_structures/vectorVoid/tests/vectorVoid-test.h"
 //#include "libs/data_structures/matrix/tests/matrix-test.h"
 //#include "libs/tasks/5d/tests/5d-test.h"
-#include "libs/string/tests/string_-test.h"
-#include "libs/tasks/5e/tests/5e-test.h"
+//#include "libs/string/tests/string_-test.h"
+//#include "libs/tasks/5e/tests/5e-test.h"
 
 int main() {
 //    test_vector();
 //    test_vectorVoid();
 //    test_matrix();
 //    test_5d();
-
-    test_string_();
-    test_5e();
+//    test_string_();
+//    test_5e();
 
     return 0;
 }
